db27b7fe2cc21def833a66d0df73734425b02972
nimish <nimishsantosh107@icloud.com>
2020-11-03 18:13:39 +0530

    setup.py added

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+requirements = [
+    'box2d-py~=2.3.5',
+    'gym>=0.17.1',
+    'numpy>=1.18',
+]
+
+setup(
+    name='lunar_lander_custom',
+    version='0.0.1',
+    python_requires='>=3.5',
+    description='LunarLander with Mixed Action Space',
+    url='git@github.com:nimishsantosh107/LunarLander-Custom.git',
+    author='Nimish Santosh',
+    author_email='nimishsantosh107@gmail.com',
+    install_requires=requirements,
+    license='unlicense',
+    packages=['lunar_lander_custom'],
+    zip_safe=False
+)
